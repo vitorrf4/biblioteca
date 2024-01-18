082f1dcaeb7a17d8d580bed88f6abcf3e5c7ecfa
vitorrf4 <vitorrf4@gmail.com>
2024-01-18 19:11:57 -0300

    feat: add genero class

---
+++ Backend/Genero.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1;
+
+public class Genero
+{
+	public string Nome { get; set; }
+
+    public Genero(string nome)
+    {
+        Nome = nome;
+    }
+}
--- Backend/Livro.cs
+++ Backend/Livro.cs
@@ -1,17 +1,20 @@
-namespace WebApplication1
-{
-    public class Livro
-    {
-        public string Titulo { get; set; }
-        public string Autor { get; set; }
-        public DateOnly DataPublicacao { get; set; }
-        public List<Genero> Generos { get; set; }
-        public int Copias { get; set; }
-        
+namespace WebApplication1;
 
-        public Livro(string titulo)
-        {
-            Titulo = titulo;
-        }
+public class Livro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public DateOnly DataPublicacao { get; set; }
+    public List<Genero> Generos { get; set; }
+    public int Copias { get; set; }
+
+
+    public Livro(string titulo, string autor, DateOnly dataPublicacao, List<Genero> generos, int copias = 0)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        DataPublicacao = dataPublicacao;
+        Generos = generos;
+        Copias = copias;
     }
 }
