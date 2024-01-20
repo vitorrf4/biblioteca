793ba39bd8c9c8ec57a3f0a9fda08844614804f6
vitorrf4 <vitorrf4@gmail.com>
2024-01-19 21:51:46 -0300

    feat: add cadastrar route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListarComponent} from "./components/listar/listar.component";
+import {CadastrarComponent} from "./components/cadastrar/cadastrar.component";
 
 const routes: Routes = [
-  { path: "listar", component: ListarComponent }
+  { path: "listar", component: ListarComponent },
+  { path: "cadastrar", component: CadastrarComponent }
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { ListarComponent } from './components/listar/listar.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarComponent
+    ListarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
