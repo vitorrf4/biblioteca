dd5053cfe36ceb42fa972d47a8ccf6d55b858fdf
vitorrf4 <vitorrf4@gmail.com>
2024-01-21 18:55:25 -0300

    refactor: update filter search

--- Frontend/src/app/components/filtro-livros/filtro-livros.component.ts
+++ Frontend/src/app/components/filtro-livros/filtro-livros.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Livro} from "../../models/livro";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-filtro-livros',
@@ -9,13 +10,27 @@ import {Livro} from "../../models/livro";
 export class FiltroLivrosComponent {
   @Input() allLivros : Livro[] = [];
   @Output() onSearch = new EventEmitter<Livro[]>;
+  queryForm : FormGroup;
 
-  buscarLivro(query: string) {
-    const tituloQuery = query.toLowerCase();
+  constructor(form: FormBuilder) {
+    this.queryForm = form.group({
+      titulo: '',
+      autor: '',
+      dataInicial: null,
+      dataFinal: null,
+      areThereCopies: true
+    });
+  }
+
+  buscarLivro() {
+    const filtro = this.queryForm.value;
 
     const livrosFiltrados = this.allLivros.filter(l => {
       const titulo = l.titulo.toLowerCase();
-      return titulo.search(tituloQuery) != -1;
+      const autor = l.autor.toLowerCase();
+
+      return titulo.search(filtro.titulo) != -1 &&
+        autor.search(filtro.autor) != -1;
     });
 
     this.onSearch.emit(livrosFiltrados);
