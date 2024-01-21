dc60ae3f9103c25a462ac71ff390a725eecec8df
vitorrf4 <vitorrf4@gmail.com>
2024-01-21 20:03:15 -0300

    refactor: add filter by available copies

--- Frontend/src/app/components/filtro-livros/filtro-livros.component.ts
+++ Frontend/src/app/components/filtro-livros/filtro-livros.component.ts
@@ -1,38 +1,55 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Livro} from "../../models/livro";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {GenerosService} from "../../services/generos.service";
+import {Genero} from "../../models/genero";
 
 @Component({
   selector: 'app-filtro-livros',
   templateUrl: './filtro-livros.component.html',
   styleUrls: ['./filtro-livros.component.css']
 })
-export class FiltroLivrosComponent {
+export class FiltroLivrosComponent implements OnInit {
   @Input() allLivros : Livro[] = [];
   @Output() onSearch = new EventEmitter<Livro[]>;
-  queryForm : FormGroup;
+  queryForm!: FormGroup;
+  generos: Genero[] = [];
 
-  constructor(form: FormBuilder) {
-    this.queryForm = form.group({
+  constructor(private service: GenerosService,
+              private form: FormBuilder) { }
+
+  ngOnInit() {
+    this.service.getAll().subscribe(res => {
+      this.generos = res;
+    });
+
+    this.queryForm = this.form.group({
       titulo: '',
       autor: '',
       dataInicial: null,
       dataFinal: null,
-      areThereCopies: true
+      areThereCopies: false,
+      generos: []
     });
   }
 
   buscarLivro() {
-    const filtro = this.queryForm.value;
+    const filtros = this.queryForm.value;
 
     const livrosFiltrados = this.allLivros.filter(l => {
       const titulo = l.titulo.toLowerCase();
       const autor = l.autor.toLowerCase();
 
-      return titulo.search(filtro.titulo) != -1 &&
-        autor.search(filtro.autor) != -1;
+      return titulo.search(filtros.titulo) != -1
+      && autor.search(filtros.autor) != -1
+      && l.copias >= filtros.areThereCopies
+        // && l.generos.includes(filtros.generos);
     });
 
     this.onSearch.emit(livrosFiltrados);
+  }
+
+  limparFiltros() {
+    this.onSearch.emit(this.allLivros);
   }
 }
